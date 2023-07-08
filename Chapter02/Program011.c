3151d2b99c0bc15c0226befe3be12b5467acfa1c
sumanvashisth2000 <45479082+sumanvashisth2000@users.noreply.github.com>
2023-07-08 11:23:20 +0530

    Create First Commit Program 11

---
+++ Chapter02/Program011.c
@@ -0,0 +1,12 @@
+// Lecture 11.1 How to use if Condition
+#include<stdio.h>
+int main(){
+    int num;
+    printf("Enter the number less than 10 : ");
+    scanf("%d",&num);
+    
+    if (num<=10)
+        printf("you are a good person cause u entered :%d", num);
+
+        return 0;
+}
