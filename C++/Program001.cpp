ebc40a00525f498d3659595f4829a6f602fcf2e4
sumanvashisth <45479082+sumanvashisth2000@users.noreply.github.com>
2023-07-14 07:41:54 +0530

    Create First Commit Program 1

---
+++ C++/Program001.cpp
@@ -0,0 +1,11 @@
+//Program 1 Wite A Program to Print Hello World and Sare Jahan se Achacha Hindustan Hamara
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    cout<<"Hello World ,Learn C++ ";
+    cout<<"\nSare Jahan se Achacha Hindustan Hamara";
+
+    return 0;
+}
